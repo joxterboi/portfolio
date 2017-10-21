bc772ff0301f69e618fec6350f754fd36e482d12
joxterboi <joa123kim456@gmail.com>
2017-10-21 14:09:11 +0200

    Finished photo page

--- script.js
+++ script.js
@@ -23,7 +23,45 @@ function backgroundChange() {
 
 
 //Photo menu hover blur
-function blur() {
-	document.getElementByclassName('img')[0].classList.add("glass");
+//1
+function hoverGlass() {
 	console.log("Hello");
+	var blurDiv = document.getElementById("masterBlurer");
+	blurDiv.className += " blurBackground";
+	document.getElementById("seeMore").getElementsByTagName("p")[0].style.opacity = "1";
+}
+
+function hoverGlassRemove() {
+	console.log("Bye bye");
+	var blurDiv = document.getElementById("masterBlurer");
+	blurDiv.className -= " blurBackground";
+	document.getElementById("seeMore").getElementsByTagName("p")[0].style.opacity = "0";
+}
+//2
+function hoverGlass2() {
+	console.log("Hello");
+	var blurDiv = document.getElementById("masterBlurer2");
+	blurDiv.className += " blurBackground";
+	document.getElementById("seeMore2").getElementsByTagName("p")[0].style.opacity = "1";
+}
+
+function hoverGlassRemove2() {
+	console.log("Bye bye");
+	var blurDiv = document.getElementById("masterBlurer2");
+	blurDiv.className -= " blurBackground";
+	document.getElementById("seeMore2").getElementsByTagName("p")[0].style.opacity = "0";
+}
+//3
+function hoverGlass3() {
+	console.log("Hello");
+	var blurDiv = document.getElementById("masterBlurer3");
+	blurDiv.className += " blurBackground";
+	document.getElementById("seeMore3").getElementsByTagName("p")[0].style.opacity = "1";
+}
+
+function hoverGlassRemove3() {
+	console.log("Bye bye");
+	var blurDiv = document.getElementById("masterBlurer3");
+	blurDiv.className -= " blurBackground";
+	document.getElementById("seeMore3").getElementsByTagName("p")[0].style.opacity = "0";
 }
