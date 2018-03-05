f6f045ed4a7af5461d190224411b8088504d7826
joxterboi <joa123kim456@gmail.com>
2018-03-05 22:31:53 +0100

    Just some minor code cleenup

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function backgroundCounter() {
 	setTimeout(function() {			
 		if (backgroundNumber < 5) {
 			backgroundNumber =  backgroundNumber + 1;
-			console.log(backgroundNumber)
 		} else {
 			backgroundNumber = 0;
 		}
@@ -16,7 +15,7 @@ function backgroundChange() {
 		document.body.style.backgroundImage = "url(bilder/home/" + backgroundNumber +".jpg)";
 		backgroundCounter();
 	} else {
-	backgroundCounter();
+		backgroundCounter();
 	}
 }
 
@@ -24,42 +23,36 @@ function backgroundChange() {
 //Photo menu hover blur
 //1
 function hoverGlass() {
-	console.log("Hello");
 	var blurDiv = document.getElementById("masterBlurer");
 	blurDiv.className += " blurBackground";
 	document.getElementById("seeMore").getElementsByTagName("p")[0].style.opacity = "1";
 }
 
 function hoverGlassRemove() {
-	console.log("Bye bye");
 	var blurDiv = document.getElementById("masterBlurer");
 	blurDiv.className -= " blurBackground";
 	document.getElementById("seeMore").getElementsByTagName("p")[0].style.opacity = "0";
 }
 //2
 function hoverGlass2() {
-	console.log("Hello");
 	var blurDiv = document.getElementById("masterBlurer2");
 	blurDiv.className += " blurBackground";
 	document.getElementById("seeMore2").getElementsByTagName("p")[0].style.opacity = "1";
 }
 
 function hoverGlassRemove2() {
-	console.log("Bye bye");
 	var blurDiv = document.getElementById("masterBlurer2");
 	blurDiv.className -= " blurBackground";
 	document.getElementById("seeMore2").getElementsByTagName("p")[0].style.opacity = "0";
 }
 //3
 function hoverGlass3() {
-	console.log("Hello");
 	var blurDiv = document.getElementById("masterBlurer3");
 	blurDiv.className += " blurBackground";
 	document.getElementById("seeMore3").getElementsByTagName("p")[0].style.opacity = "1";
 }
 
 function hoverGlassRemove3() {
-	console.log("Bye bye");
 	var blurDiv = document.getElementById("masterBlurer3");
 	blurDiv.className -= " blurBackground";
 	document.getElementById("seeMore3").getElementsByTagName("p")[0].style.opacity = "0";
